24545ca213b8b7b37a36b0471d82d4b2cd773cb8
Kell Sanders <Kell.Sanders@target.com>
2024-12-18 15:52:00 -0600

    rebuilt history section, now one page instead of individual ones

--- pages/_meta.js
+++ pages/_meta.js
@@ -15,25 +15,4 @@ export default {
   index: {
     display: 'hidden',
   },
-  letterboxd: {
-    title: 'Letterboxd',
-    type: 'menu',
-    items: {
-      becca: {
-        title: 'Becca',
-        href: 'https://www.letterboxd.com/anowlscasket',
-        newWindow: true
-      },
-      kell: {
-        title: 'Kell',
-        href: 'https://www.letterboxd.com/killedgier',
-        newWindow: true
-      },
-      tony: {
-        title: 'Tony',
-        href: 'https://www.letterboxd.com/echtoo',
-        newWindow: true
-      }
-    }
-  }
 }
